843c7a1a3fd6a85e96a7d6c97bfa8794b2812bed
marianosgv <162711233+marianosgv@users.noreply.github.com>
2026-06-02 07:22:11 -0400

    PROGRAMA CON JSON

--- PROYECTO_INTRO_PROGRA.py
+++ PROYECTO_INTRO_PROGRA.py
@@ -25,6 +25,56 @@ dispositivos = {
     "DIS002": ["Electrocardiógrafo", 7, 8, "equipos",12],
     "DIS003": ["Oxímetro", 12, 20, "unidades",24]
 }
+import json
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ARCHIVO = os.path.join(BASE_DIR, "inventario_medico.json")
+
+# =========================
+# CARGAR DATOS GUARDADOS
+# =========================
+if os.path.exists(ARCHIVO):
+    with open(ARCHIVO, "r", encoding="utf-8") as archivo:
+        datos = json.load(archivo)
+
+        medicamentos = datos.get("medicamentos", {})
+        instrumentos = datos.get("instrumentos", {})
+        dispositivos = datos.get("dispositivos", {})
+
+else:
+    # DATOS INICIALES SOLO SI NO EXISTE EL ARCHIVO
+    medicamentos = {
+        "MED001": ["Paracetamol", 10, 20, "tabletas"],
+        "MED002": ["Ibuprofeno", 80, 10, "cápsulas"],
+        "MED003": ["Amoxicilina", 40, 15, "frascos"]
+    }
+
+    instrumentos = {
+        "INS001": ["Bisturí", 9, 8, "unidades"],
+        "INS002": ["Pinza quirúrgica", 16, 15, "unidades", 20],
+        "INS003": ["Termómetro digital", 4, 5, "unidades", 40]
+    }
+
+    dispositivos = {
+        "DIS001": ["Monitor cardíaco", 5, 9, "equipos", 30],
+        "DIS002": ["Electrocardiógrafo", 7, 8, "equipos", 12],
+        "DIS003": ["Oxímetro", 12, 20, "unidades", 24]
+    }
+
+# =========================
+# FUNCIÓN PARA GUARDAR
+# =========================
+def guardar_datos():
+    datos = {
+        "medicamentos": medicamentos,
+        "instrumentos": instrumentos,
+        "dispositivos": dispositivos
+    }
+
+    with open(ARCHIVO, "w", encoding="utf-8") as archivo:
+        json.dump(datos, archivo, indent=4, ensure_ascii=False)
+
+
 # codigo : [nombre, disponible, cantidad mínima, unidad]
 def agregar_med(codigo_med):
    medicamentos[codigo_med] = []
@@ -45,9 +95,9 @@ def agregar_inst(codigo_inst):
    reutilizable = input("¿El instrumento es reutilizable?")
    if reutilizable.lower() == "si":
       total = disponibilidad
-      instrumentos[codigo_inst] = [instrumento, stock_min, total, unidad, disponibilidad]
+      instrumentos[codigo_inst] = [instrumento, disponibilidad, stock_min, unidad, total]
    if reutilizable.lower()== "no":
-      instrumentos[codigo_inst] = [instrumento, stock_min, total, unidad]
+      instrumentos[codigo_inst] = [instrumento, disponibilidad, stock_min, unidad]
    print(f"Se agrego correctamente: {instrumentos[codigo_inst]}")
 
 def agregar_disp(codigo_disp):
@@ -57,7 +107,7 @@ def agregar_disp(codigo_disp):
    total = disponibilidad
    unidad = input("Ingrese la Unidad de medida (EJ: Unidades, Equipos, etc): ")
    stock_min = int(input(f"Ingrese la cantidad mínima de disponibilidad de {unidad}: "))
-   dispositivos[codigo_disp] = [dispositivo, stock_min, total, unidad,disponibilidad]
+   dispositivos[codigo_disp] = [dispositivo, disponibilidad, stock_min, unidad, total]
    print(f"Se agrego correctamente: {dispositivos[codigo_disp]}")
 
 def alerta_med(codigo):
@@ -90,8 +140,8 @@ def menu_ingresos():
    print("\n╔═════════════════════════════════════╗")
    print("║    ¿Ingreso nuevo o devolución?     ║")
    print("╠═════════════════════════════════════╣")
-   print("║         1. INGRESO NUEVO            ║")
-   print("║         2. DEVOLUCIÓN               ║")
+   print("║         1. DEVOLUCIÓN               ║")
+   print("║         2. INGRESO NUEVO            ║")
    print("╚═════════════════════════════════════╝")
 
 
@@ -121,9 +171,19 @@ while option != 0:
    option = int(input("Ingrese la opción a realizar: "))
    match option:
       case 1:
-         print("--- MEDICAMENTOS ---", medicamentos)
-         print("--- DISPOSITIVOS ---", dispositivos)
-         print("--- INSTRUMENTOS ---", instrumentos)
+         print("siendo la estructura: # codigo : [nombre, disponible, cantidad mínima, unidad, cantidad total]")
+         print("--- MEDICAMENTOS ---")
+         for c_m, v_m in medicamentos.items():
+            print(f"{c_m}: {v_m}")
+
+         print("--- INSTRUMENTOS ---")
+         for c_i, v_i in instrumentos.items():
+            print(f"{c_i}: {v_i}")
+
+         print("--- DISPOSITIVOS ---")
+         for c_d, v_d in dispositivos.items():
+            print(f"{c_d}: {v_d}")
+
       case 2:
          op2 = None
          while op2 != 0:
@@ -145,6 +205,7 @@ while option != 0:
                         break
                      else:
                         agregar_med(codigo_med)
+                        guardar_datos()
                         break
                case 2:
                   while op2 != 0:
@@ -154,6 +215,7 @@ while option != 0:
                         break
                      else:
                         agregar_inst(codigo_inst)
+                        guardar_datos()
                         break
 
                case 3:
@@ -164,6 +226,7 @@ while option != 0:
                         break
                      else:
                         agregar_disp(codigo_disp)
+                        guardar_datos()
                         break
 
       case 3:
@@ -180,18 +243,18 @@ while option != 0:
             op3 = int(input("Ingrese la opción de lo que desea añadir: "))
             match op3:
                case 1:
-                  menu_ingresos()
-                  deseo = int(input("Ingrese la opción de lo que desea añadir: "))                  
                   codigo_med = input("Ingrese el codigo del Medicamento: ").upper()
                   if codigo_med in medicamentos:
-                     cantidad = int(input(f"Ingrese la cantidad de devoluvión de {medicamentos[codigo_med][3]} que ingresa: "))
+                     cantidad = int(input(f"Ingrese la cantidad de reposición de {medicamentos[codigo_med][3]} que ingresa: "))
                      medicamentos[codigo_med][1] += cantidad
+                     guardar_datos()
 
                   if not codigo_med in  medicamentos: 
                      print("El medicamento no está registrado, ¿Desea agregarlo?: ")
                      new_agree = input("¿si o no?: ")
                      if new_agree.lower() == "si":
                         agregar_med(codigo_med)
+                        guardar_datos()
             
                case 2:
                   menu_ingresos()
@@ -199,40 +262,43 @@ while option != 0:
                   codigo_inst = input("Ingrese el codigo del Instrumento: ").upper()
                   if codigo_inst in instrumentos:
                      if deseo==1:
-                        cantidad = int(input(f"Ingrese la cantidad de devoluvión de {instrumentos[codigo_inst][3]} que ingresa: "))
+                        cantidad = int(input(f"Ingrese la cantidad de devolución de {instrumentos[codigo_inst][3]} que ingresa: "))
                         instrumentos[codigo_inst][1] += cantidad
+                        guardar_datos()
                      if deseo==2:
                         cantidad = int(input(f"Ingrese la cantidad de {instrumentos[codigo_inst][3]} nuevos que ingresan al inventario: "))
-                        instrumentos[codigo_inst][4] += cantidad
                         instrumentos[codigo_inst][1] += cantidad
+                        if len(instrumentos[codigo_inst]) == 5:
+                           instrumentos[codigo_inst][4] += cantidad
+                           
+                        guardar_datos()
                   if not codigo_inst in  instrumentos: 
                      print("El instrumento no está registrado, ¿Desea agregarlo?: ")
                      new_agree = input("¿si o no?: ")
                      if new_agree.lower() == "si":
                         agregar_inst(codigo_inst)
+                        guardar_datos()
 
                case 3:
-                  print("\n╔════════════════════════════════════╗")
-                  print("║    ¿Ingreso nuevo o devolución?     ║")
-                  print("╠═════════════════════════════════════╣")
-                  print("║         1. INGRESO NUEVO            ║")
-                  print("║         2. DEVOLUCIÓN               ║")
-                  print("╚═════════════════════════════════════╝")
+                  menu_ingresos()
                   deseo = int(input("Ingrese la opción de lo que desea añadir: "))                  
                   codigo_disp = input("Ingrese el codigo del Dispositivo: ").upper()
                   if codigo_disp in dispositivos:
                      if deseo==1:
-                        cantidad = int(input(f"Ingrese la cantidad de devoluvión de {dispositivos[codigo_disp][3]} que ingresa: "))
+                        cantidad = int(input(f"Ingrese la cantidad de devolución de {dispositivos[codigo_disp][3]} que ingresa: "))
                         dispositivos[codigo_disp][1] += cantidad
+                        guardar_datos()
                      if deseo==2:
                         cantidad = int(input(f"Ingrese la cantidad de {dispositivos[codigo_disp][3]} nuevos que ingresan al inventario: "))
                         dispositivos[codigo_disp][4] += cantidad
                         dispositivos[codigo_disp][1] += cantidad
+                        guardar_datos()
                   if not codigo_disp in  dispositivos: 
                      print("El dispositivo no está registrado, ¿Desea agregarlo?: ")
                      new_agree = input("¿si o no?: ")
                      if new_agree.lower() == "si":
                         agregar_disp(codigo_disp)
+                        guardar_datos()
 
       case 4:
          print("\n╔═════════════════════════╗")
@@ -249,23 +315,35 @@ while option != 0:
                codigo_med = input("Ingrese el codigo del medicamento: ").upper()
                if codigo_med in medicamentos:
                   unidades_back = int(input(f"¿Cuantxs {medicamentos[codigo_med][3]} de {medicamentos[codigo_med][0]} se van a retirar?: "))
-                  medicamentos[codigo_med][1] -= unidades_back
-                  print(f"Quedan {medicamentos[codigo_med][1]} {medicamentos[codigo_med][3]} de {medicamentos[codigo_med][0]} disponibles")
-                  alerta_med(codigo_med)
+                  if unidades_back > medicamentos[codigo_med][1]:
+                     print("No hay suficiente stock.")
+                  else:
+                     medicamentos[codigo_med][1] -= unidades_back
+               print(f"Quedan {medicamentos[codigo_med][1]} {medicamentos[codigo_med][3]} de {medicamentos[codigo_med][0]} disponibles")
+               alerta_med(codigo_med)
+               guardar_datos()
             case 2:
                codigo_inst = input("Ingrese el codigo del Intrumento: ").upper()
                if codigo_inst in instrumentos:
                   unidades_back = int(input(f"¿Cuantxs {instrumentos[codigo_inst][3]} de {instrumentos[codigo_inst][0]} se van a retirar?: "))
-                  instrumentos[codigo_inst][1] -= unidades_back
+                  if unidades_back > instrumentos[codigo_inst][1]:
+                     print("No hay suficiente stock.")
+                  else:
+                     instrumentos[codigo_inst][1] -= unidades_back
                   print(f"Quedan {instrumentos[codigo_inst][1]} {instrumentos[codigo_inst][3]} de {instrumentos[codigo_inst][0]} disponibles")
                   alerta_inst(codigo_inst)
+                  guardar_datos()
             case 3:
                codigo_disp = input("Ingrese el codigo del Dispositivo: ").upper()
                if codigo_disp in dispositivos:
                   unidades_back = int(input(f"¿Cuantxs {dispositivos[codigo_disp][3]} de {dispositivos[codigo_disp][0]} se van a retirar?: "))                 
-                  dispositivos[codigo_disp][1] -= unidades_back
+                  if unidades_back > dispositivos[codigo_disp][1]:
+                     print("No hay suficiente stock.")
+                  else:
+                     dispositivos[codigo_disp][1] -= unidades_back
                   print(f"Quedan {dispositivos[codigo_disp][1]} {dispositivos[codigo_disp][3]} de {dispositivos[codigo_disp][0]} disponibles")
                   alerta_disp(codigo_disp)
+                  guardar_datos()
 
       case 5:
          print("⚠️ ALERTAS DE STOCK BAJO: ")
@@ -304,6 +382,7 @@ while option != 0:
                   if papelera in medicamentos:
                      del medicamentos[papelera]
                      print("Medicamento eliminado por completo.")
+                     guardar_datos()
                case 2:
                   menu_papelera()
                   deseo = int(input("Escriba la opción a realizar: "))
@@ -312,12 +391,14 @@ while option != 0:
                      if deseo == 1:
                         del instrumentos[papelera]
                         print("Instrumento eliminado por completo.")
+                        guardar_datos()
                      elif deseo == 2:
                         baja = int(input("¿Cuántas unidades se dañaron/perdieron definitivamente?: "))
-                        instrumentos[papelera][2] -= baja 
+                        instrumentos[papelera][1] -= baja 
                         if len(instrumentos[papelera]) == 5:
                            instrumentos[papelera][4] -= baja 
                            print(f"Baja registrada. Inventario actual: {instrumentos[papelera]}")
+                           guardar_datos()
                case 3:
                   menu_papelera()
                   deseo = int(input("Escriba la opción a realizar: "))
@@ -326,8 +407,10 @@ while option != 0:
                      if deseo == 1:
                         del dispositivos[papelera]
                         print("Dispositivo eliminado por completo.")
-                  elif deseo == 2:
-                     baja = int(input("¿Cuántas unidades se dañaron/perdieron definitivamente?: "))
-                     dispositivos[papelera][2] -= baja # Resta de disponible
-                     dispositivos[papelera][4] -= baja # Resta de total
-                     print(f"Baja registrada. Inventario actual: {dispositivos[papelera]}")
+                        guardar_datos()
+                     elif deseo == 2:
+                        baja = int(input("¿Cuántas unidades se dañaron/perdieron definitivamente?: "))
+                        dispositivos[papelera][1] -= baja # Resta de disponible
+                        dispositivos[papelera][4] -= baja # Resta de total
+                        print(f"Baja registrada. Inventario actual: {dispositivos[papelera]}")
+                        guardar_datos()
